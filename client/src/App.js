803e1375b6be259f7da5f4b078276dbc99f66385
Rich McRary <rgmcrary@gmail.com>
2017-11-15 11:55:00 -0500

    Added import and path codes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Container } from './components/Grid';
+import LoggedIn from './pages/LoggedIn';
+import PostedProjects from './pages/PostedProjects';
 import SavedProjects from './pages/SavedProjects';
 import AllProjects from './pages/AllProjects';
 import NewProject from './pages/NewProject';
@@ -11,6 +13,8 @@ const App = () => (
     <Container fluid>
       <Nav />
       <Switch>
+        <Route path="/posted" component={PostedProjects} />
+        <Route path="/loggedin" component={LoggedIn} />
         <Route path="/saved" component={SavedProjects} />
         <Route path="/new" component={NewProject} />
         <Route path="/" component={AllProjects} />
