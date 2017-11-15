4dea58295e7e83bd781224bf836b9dd6dd233781
Rich McRary <rgmcrary@gmail.com>
2017-11-15 11:55:49 -0500

    added Post route and edited Saved route code

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,27 +1,42 @@
-const db = require("../models");
+const db = require('../models');
 
 // Defining methods for the projectsController
 module.exports = {
-
   // Save a project
-    create: (req, res) => {
-      console.log(JSON.stringify(req.body))
+  create: (req, res) => {
+    console.log(JSON.stringify(req.body));
     db.Project
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
 
-  // Display all favortied/replied to projects
-    findAll: (req, res) =>  {
+  // Display all projects
+  findAll: (req, res) => {
     db.Project
       .find()
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
 
+    // Display all favorited/replied to projects
+  findSaved: (req, res) => {
+    db.Project
+      .findById({ userId })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+    // Display all posted projects
+  findPosted: (req, res) => {
+    db.Project
+      .findById({ userId })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
   // Delete a saved project
-  remove: (req, res) =>  {
+  remove: (req, res) => {
     db.Project
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
@@ -29,5 +44,3 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   }
 };
-
-
