e5b2db03f2585ac0faa699317cef77f8091814f7
Rich McRary <rgmcrary@gmail.com>
2017-11-17 17:39:29 -0500

    Renaming file names

--- client/src/components/Save/SaveBtn.js
+++ client/src/components/Save/SaveBtn.js
@@ -1,13 +1,11 @@
-import React from "react";
+import React from 'react';
 import "./SaveBtn.css";
 // import "../../assets/images";
 
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
-const SaveBtn = props => (
-  <span className="save-btn" {...props}>
-
+export const SaveBtn = props => (
+  <span className="save-btn" {...props} >
   </span>
 );
 
-export default SaveBtn;
