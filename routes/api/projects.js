6f19053457967d1a5740c650012283a563a6efdf
Rich McRary <rgmcrary@gmail.com>
2017-11-17 17:43:48 -0500

    Created routing

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -1,13 +1,24 @@
 const router = require('express').Router();
 const projectsController = require('../../controllers/projectsController');
 
+// Create new project post and returns all project posts
 router
   .route('/')
   .post(projectsController.create)
-  .get(projectsController.findAll)
-  .get(projectsController.findPosted)
-  .get(projectsController.findSaved);
+  .get(projectsController.findAll);
 
-router.route('/:id').delete(projectsController.remove);
+  // Pulls saved/replied to projects
+router.route('/saved').get(projectsController.findSaved);
+
+// Pulls all posted projects by user
+// router
+// .route('/:id')
+// .get(projectsController.findPosted);
+
+// Saves or removes the save on a project
+// router
+//   .route('/saved/:id')
+//   .post(projectsController.saveProject)
+//   .delete(projectsController.remove);
 
 module.exports = router;
