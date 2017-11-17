7df6d7f185230fe68433ee67bd93972bff45774b
Rich McRary <rgmcrary@gmail.com>
2017-11-17 17:42:03 -0500

    Created new routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,15 +1,21 @@
-
+import axios from 'axios';
 
 export default {
+  // Gets project details
+  getDetails: searchValues => {},
+
   // Gets all projects
-  getProject: searchValues => {
-    var queryURL = queryURLBase + searchValues.title;
+  getProjects: searchValues => {
+    return axios.get('/api/projects/');
+  },
+
+  // Gets saved projects
+  getSavedProjects: searchValues => {
+    return axios.get('/api/projects/saved');
   },
 
   // Gets all posted projects
-  getPosted: searchValues => {
-    var queryURL = queryURLBase + searchValues.title;
-  },
+  getPosted: searchValues => {},
 
   // Deletes the saved project with the given id
   deleteProject: id => {
