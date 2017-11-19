2bbec130bd6b26c9817a73e7ff543c1ff66f8b6d
Rich McRary <rgmcrary@gmail.com>
2017-11-19 13:09:04 -0500

    Minor code edits

--- models/user.js
+++ models/user.js
@@ -1,15 +1,22 @@
-module.exports = function(sequelize, DataTypes) {
-  const User = sequelize.define('User', {
-    user_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    user_email: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    }
-  });
+module.exports = (sequelize, DataTypes) => {
+const User = sequelize.define('User', {
+  user_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  user_email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
 
-  return User;
+User.associate = (models) => {
+  // Associating user with Projects
+  // When a user is deleted, also delete any associated Projects
+  User.hasMany(models.Project, {
+    onDelete: "cascade"
+  });
+};
+
+return User;
 };
