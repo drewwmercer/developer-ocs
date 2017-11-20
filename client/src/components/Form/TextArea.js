b69dc00c55cc42c2a3c8ab35ff1fd90b94a5231a
Rich McRary <rgmcrary@gmail.com>
2017-11-20 16:40:26 -0500

    Adjusted text area size

--- client/src/components/Form/TextArea.js
+++ client/src/components/Form/TextArea.js
@@ -2,5 +2,5 @@ import React from "react";
 
 export const TextArea = props =>
   <div className="form-group">
-    <textarea className="form-control" rows="20" {...props} />
+    <textarea className="form-control" rows="5" {...props} />
   </div>;
