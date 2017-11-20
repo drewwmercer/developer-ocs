f12b6b922a4b79c5b7e207ac822009f725c4d2cd
Rich McRary <rgmcrary@gmail.com>
2017-11-20 12:04:58 -0500

    Added association

--- models/saved.js
+++ models/saved.js
@@ -1,12 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
   const Saved = sequelize.define('Saved', {
-    project_id: {
-      type: DataTypes.INTEGER
-    },
     user_id: {
       type: DataTypes.INTEGER
     }
   });
+
+   Saved.associate = (models) => {
+     Saved.belongsTo(models.Project, {
+       targetKey: 'id',
+       foreignKey: {
+         allowNull: false
+       }
+     });
+   };
 
   return Saved;
 };
