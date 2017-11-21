88fcffb27dad8248b8ce955de0a211094947894a
Rich McRary <rgmcrary@gmail.com>
2017-11-20 20:56:11 -0500

    Update axios apis

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -9,21 +9,38 @@ export default {
     return axios.get('/api/projects/');
   },
 
-  // Gets saved projects
-  getSavedProjects: searchValues => {
-    return axios.get('/api/projects/saved');
+  // Saves a new project post
+  createProject: projectData => {
+    return axios.post('/api/projects', projectData);
   },
 
-  // Gets all posted projects
-  getPosted: searchValues => {},
+  // Edit a project post
+  editPostedProject: projectData => {
+    return axios.post('/api/projects/edit', projectData);
+  },
 
-  // Deletes the saved project with the given id
-  deleteProject: id => {
+  // Delete a project post
+  removePostedProject: id => {
     return axios.delete('/api/projects/' + id);
   },
 
-  // Saves a project to the database
+  // Gets all posted projects
+  getPosted: id => {
+    return axios.get('/api/projects/3');
+  },
+
+  // Gets saved projects
+  getSavedProjects: id => {
+    return axios.get('/api/projects/saved/3'); // ToDo: need to update with dynamic userId axios.get('/api/projects/saved/ + searchValues')
+  },
+
+  // Deletes the saved project with the given id
+  deleteProject: id => {
+    return axios.delete('/api/projects/saved/' + id);
+  },
+
+  // Favoriting a project to the database
   saveProject: projectData => {
-    return axios.post('/api/projects', projectData);
+    return axios.post('/api/projects/saved', projectData);
   }
 };
