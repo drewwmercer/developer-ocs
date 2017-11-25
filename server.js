2260adf40a9984d63afe70a749f30180288289f6
Rich McRary <rgmcrary@gmail.com>
2017-11-25 17:31:18 -0500

    Configured server to use Passport

--- server.js
+++ server.js
@@ -4,7 +4,9 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
+const passport = require('passport');
+
+require('./controllers/loginController')(passport);
 
 // Sets up the Express App
 // =============================================================
@@ -20,10 +22,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+app.use(passport.initialize());
+
 // Serve up static assets
 app.use(express.static("client/build"));
 
 // Add routes, both API and view
+const routes = require('./routes');
 app.use(routes);
 
 // Start the API server
