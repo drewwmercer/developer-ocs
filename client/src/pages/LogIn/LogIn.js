604acdd8d38c30cecd16f8cf44696ebafc9696b6
Rich McRary <rgmcrary@gmail.com>
2017-11-26 19:15:24 -0500

    added login buttons and styled

--- client/src/pages/LogIn/LogIn.js
+++ client/src/pages/LogIn/LogIn.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
 import { Panel } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { List, ListItem } from '../../components/List';
 import { Col, Row } from '../../components/Grid';
 import { FormBtn } from '../../components/Form';
+
 import './LogIn.css';
 import API from '../../utils/API';
 
 class LogIn extends Component {
+
   render() {
-    return (
-
+    return(
       // Creates LogIn
-
-      // Google+ LogIn Button
-      <a href="http://localhost:3000/auth/google" className="btn btn-danger">
-        <span className="fa fa-google-plus" /> Google
-      </a>
-
-      // Github LogIn Button
+      <div>
+        {/* Google+ LogIn Button */}
+        <div className="img__wrap">
+          <a href="/auth/google" rel="external" className="btn btn-danger google">
+            <span className="fa fa-google-plus fa-2x" />
+          </a>
+          <p className="img__description">Google+</p>
+        </div>
+        &nbsp; &nbsp;
+        {/* Github LogIn Button */}
+        <div className="img__wrap">
+          <a href="/auth/github" rel="external" className="btn btn-default github">
+            <span className="fa fa-github fa-2x" />
+          </a>
+          <p className="img__description">Github</p>
+        </div>
+      </div>
     );
   }
 }
