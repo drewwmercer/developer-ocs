6e6e31afde9522f05c284ec43924503f3167f852
Drew Mercer <japanexpress@gmail.com>
2017-11-30 20:04:20 -0500

    Adding user seed data

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO Users
+    (user_name, user_email, github_id, google_id, active)
+VALUES
+    ('Drew Mercer', 'awmercer@gmail.com', 20029745, 1117264685222156322242, true);
+    ('Rich McRary', '', , ,true);
+
+
