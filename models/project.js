1a3d227db56dbf603ba87185cdf2363bfe32518f
Drew Mercer <japanexpress@gmail.com>
2017-11-30 20:47:20 -0500

    Updating data type for active flag

--- models/project.js
+++ models/project.js
@@ -23,7 +23,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING
     },
     active: {
-      type: BOOLEAN,
+      type: DataTypes.BOOLEAN,
       allowNull: false,
       defaultValue: true
     }
