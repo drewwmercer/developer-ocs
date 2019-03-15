9f7668ce55d016ab2bd89f2f0d95258f82948ad6
Drew Mercer <japanexpress@gmail.com>
2019-03-15 09:20:16 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,1 @@
-const path = require('path');
-module.exports = {
-  entry: './client/index.js',
-  output: {
-    path: path.resolve('dist'),
-    filename: 'index_bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
-  }
-};
+
